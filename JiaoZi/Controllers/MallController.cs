f868a07f30788b16abefaf2cac0189d462201d59
Yxinl <779803938@qq.com>
2018-12-02 20:06:21 +0800

    更改

--- JiaoZi/Controllers/MallController.cs
+++ JiaoZi/Controllers/MallController.cs
@@ -18,20 +18,47 @@ namespace JiaoZi.Controllers
         // GET: Mall
         public ActionResult Index()
         {
-            //var price = imall.GetBooksByPrice();
-            //var english = imall.GetBooksBycategory("英语");
-            //var mate = imall.GetBooksBycategory("心理学");
-            //var putonghua = imall.GetBooksBycategory("普通话");
-            //var books = imall.GetBooks();
+
             Models.Mallview viewModel = new Models.Mallview
             {
-                 price = imall.GetBooksByPrice(),
-                 English = imall.GetBooksBycategory("英语"),
-                 Mate = imall.GetBooksBycategory("心理学"),
-                 Putonghua = imall.GetBooksBycategory("普通话"),
-             };
+                Price = imall.GetBooksByPrice(),
+                Chinese = imall.GetBooksBycategory("语文"),
+                Math = imall.GetBooksBycategory("数学"),
+                English = imall.GetBooksBycategory("英语"),
+                Political = imall.GetBooksBycategory("政治"),
+                Synthesize = imall.GetBooksBycategory("综合"),
+                History = imall.GetBooksBycategory("历史"),
+                Chemistry = imall.GetBooksBycategory("化学"),
+                Interview = imall.GetBooksBycategory("面试"),
+                Art = imall.GetBooksBycategory("美术"),
+                Psychic = imall.GetBooksBycategory("心理学"),
+                Pedagogy = imall.GetBooksBycategory("教育学"),
+                Hotsale = imall.GetBooksByAmount()
+            };
 
             return View(viewModel);
         }
+        
+
+        [HttpPost]
+        public ActionResult Sear()
+        {
+            string search = Request.Form["search"].ToString();
+            var Searchbooks = imall.Search(search);
+            if(Searchbooks!=null)
+            {
+                return PartialView(Searchbooks);
+            }
+            else                                    //BUG
+                return Content("<script>alert('未找到相关书籍');history.go(-1);</script>");
+        }
+
+        //查询图书  替换id 分布视图
+        //[HttpPost]
+        //public ActionResult Sear (string search)
+        //{
+        //    var Search = imall.Search(search);
+        //    return View(search);
+        //}
     }
 }
