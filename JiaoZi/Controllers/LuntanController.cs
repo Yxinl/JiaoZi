3905baca8a28012c2003327dcba62ada7e232887
吴俊琨 <@DESKTOP-GFPB4LC>
2018-12-04 19:43:34 +0800

    wjk-1

--- JiaoZi/Controllers/LuntanController.cs
+++ JiaoZi/Controllers/LuntanController.cs
@@ -10,39 +10,31 @@ namespace JiaoZi.Controllers
     public class LuntanController : Controller
     {
         jiaoziEntities db = new jiaoziEntities();
-        //Models.TieziViewModel tieziviewmodel = new Models.TieziViewModel();
+        Models.TieziViewModel tieziviewmodel = new Models.TieziViewModel();
 
         // GET: Luntan
         public ActionResult Index()
         {
             IRemark_User_Comments_Reply ir = new RRemark_User_Comments_Reply();
-            if (ir.Getsall() != null)
+          
                 return View(ir.Getsall());
-            //IRemarksbytime ir1 = new RRemarksbytime();
-            
-
-            return View();
+           
         }
         public ActionResult Index2(int id)
         {
-            //IRemarksbytime ir1 = new RRemarksbytime();
-            //if (ir1.Contentbytime() != null)
-            //    return View(ir1.Contentbytime());
-            //jiaoziEntities db = new jiaoziEntities();
-            //var list=db.Remarks.Where(b=>b.RemarkContent)
-            IRemarks ir = new RRemarks();
-            //if (ir.FindRemarksByID(id) != null)
-                return View(ir.FindRemarksByID(id));
-            //else if (ir.FindRemarksByID(id) != null)
-            //    return View(ir.FindRemarksByID(id));
-            //else if (ir.FindRemarksByID(1) != null)
-            //    return View(ir.FindRemarksByID(1));
-            //else if (ir.FindRemarksByID(4) != null)
-            //    return View(ir.FindRemarksByID(4));
-            //return View();
+           IRemarks ir = new RRemarks();
+            IRemarkcomments ire = new RRemarkcomments();
+        
+            //var remarkcomment = db.RemarkComments.OrderByDescending(p=>p.RemarkCommentID==id);
+            var remarkreply = db.RemarkReply.OrderBy(p => p.RemarkReplyID).Take(3);
+            TieziViewModel tiz = new TieziViewModel()
+            {
+                Getpinglun = ire.Getcommentsbyid(id),
+                Gethuifu = remarkreply,
+                Gettiezi = ir.FindRemarksByID(id)
 
-        }
-
-
+            };
+            return View(tiz);
+      }
     }
 }
---
+++ JiaoZi/Models/TieziViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JiaoZi.Models
+{
+    public class TieziViewModel
+    {
+        public IEnumerable<Remarks> Gettiezi { get; set; }
+        public IEnumerable<RemarkComments> Getpinglun { get; set; }
+        public IEnumerable<RemarkReply> Gethuifu { get; set; }
+    }
+}
